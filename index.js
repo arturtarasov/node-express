de63b86782ca6f30e852d6575947cfb70c67c8a1
tarasov.a <tarasov.art.ser@gmail.com>
2019-08-05 13:44:15 +0300

    add navigation, titles, routes, views

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const express = require('express')
-const path = require('path')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-const app = express()
+const app = express();
+
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
-})
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(express.static('public'));
 
-app.get('/about', (req, res) => {
-  res.render('about')
-})
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
