ce6e593f1243bd86fd389bacf346b46e417eb595
tarasov.a <tarasov.art.ser@gmail.com>
2019-08-06 17:23:20 +0300

    add other pages and logic

--- index.js
+++ index.js
@@ -1,31 +1,70 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
+const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
 
-const app = express();
-
-const homeRoutes = require('./routes/home');
-const addRoutes = require('./routes/add');
-const coursesRoutes = require('./routes/courses');
+const app = express()
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
-});
+})
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.use(express.static('public'));
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5d498bf42120b00e68346685')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
 
-app.use('/', homeRoutes);
-app.use('/add', addRoutes);
-app.use('/courses', coursesRoutes);
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+
+const PORT = process.env.PORT || 3000
+
+async function start() {
+  try {
+    const url = `mongodb://qwerty:qwerty123@ds259377.mlab.com:59377/node-express`
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'artur@mail.ru',
+        name: 'Artur',
+        cart: {items: []}
+      })
+      await user.save()
+    }
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
 
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
