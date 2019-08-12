91efe820ba5d6cdb83e6294cd1ee406b74d8f549
Artur <artur-tarasov@mail.ru>
2019-08-12 23:37:55 +0300

    user can change password in acount, added utils for hbs

--- routes/auth.js
+++ routes/auth.js
@@ -85,6 +85,31 @@ router.get('/reset', (req, res) => {
     });
 });
 
+router.get('/password/:token', async (req, res) => {
+    if (!req.params.token) {
+        return res.redirect('/auth/login');
+    }
+    try {
+        const user = await User.findOne({
+            resetToken: req.params.token,
+            resetTokenExp: {$gt: Date.now()}
+        });
+
+        if (!user) {
+            return res.redirect('/auth/login');
+        } else {
+            res.render('auth/password', {
+                title: 'Восстановить доступ',
+                error: req.flash('error'),
+                userId: user._id.toString(),
+                token: req.params.token
+            });
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
 router.post('/reset', (req, res) => {
     try {
         crypto.randomBytes(32, async (err, buffer) => {
@@ -93,7 +118,7 @@ router.post('/reset', (req, res) => {
                 return res.redirect('auth/reset');
             }
 
-            const token = buffer.token('hex');
+            const token = buffer.toString('hex');
             const candidate = await User.findOne({email: req.body.email});
 
             if (candidate) {
@@ -101,7 +126,7 @@ router.post('/reset', (req, res) => {
                 candidate.resetTokenExp = Date.now() + 60 * 60 * 1000;
                 await candidate.save();
                 await transporter.sendMail(resetEmail(candidate.email, token));
-                res.redirect('auth/login');
+                res.redirect('/auth/login');
             } else {
                 req.flash('error', 'Такого email нет');
                 res.redirect('/auth/reset');
@@ -112,5 +137,28 @@ router.post('/reset', (req, res) => {
     }
 });
 
+router.post('/password', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            _id: req.body.userId,
+            resetToken: req.body.token,
+            resetTokenExp: {$gt: Date.now()}
+        });
+
+        if (user) {
+            user.password = await bcrypt.hash(req.body.password, 10);
+            user.resetToken = undefined;
+            user.resetTokenExp = undefined;
+            await user.save();
+            req.flash('passwordSave', 'Ваш новый пароль сохранен');
+            res.redirect('/auth/login');
+        } else {
+            req.flash('loginError', 'Время жизни токена истекло');
+            res.redirect('/auth/login');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 module.exports = router;
